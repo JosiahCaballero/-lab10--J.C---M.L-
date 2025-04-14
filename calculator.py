859a3bfeecf6c069c30de6f7cf783fee61e037fe
Josiah Caballero <Josiahc.1224@gmail.com>
2025-04-14 18:40:56 -0400

    update with correct identity

--- calculator.py
+++ calculator.py
@@ -9,48 +9,11 @@ One function per operation, in order.
 """
 # First example
 import math
-def square_root(a):
-    try:
-        if a < 0:
-            raise ValueError("Cannot take square root of a negative number.")
-        return math.sqrt(a)
-    except ValueError as e:
-        return str(e)
-
-def hypotenuse(a, b):
-    try:
-        return math.hypot(a, b)
-    except Exception as e:
-        return str(e)
-
-def exponent(a, b):
-    try:
-        return a ** b
-    except Exception as e:
-        return str(e)
-
-def logarithm(a, b):
-    try:
-        if a <= 0 or a == 1:
-            raise ValueError("Base 'a' must be positive and not equal to 1.")
-        if b <= 0:
-            raise ValueError("Value 'b' must be positive.")
-        return math.log(b, a)
-    except ValueError as e:
-        return str(e)
-
-def divide(a, b):
-    try:
-        if a == 0:
-            raise ZeroDivisionError("Cannot divide by zero.")
-        return b / a
-    except ZeroDivisionError as e:
-        return str(e)
 
 def add(a, b):
     return a + b
 
-def subtraction (a, b):
+def sub(a, b):
     return a - b
 
 def mul(a, b):
@@ -69,7 +32,20 @@ def log(a, b):
 def exp(a, b):
     return a ** b
 
+def square_root(a):
+    try:
+        if a < 0:
+            raise ValueError("Cannot take square root of a negative number.")
+        return math.sqrt(a)
+    except ValueError as e:
+        return str(e)
 
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)
+    except Exception as e:
+        return str(e)
 
 
 
